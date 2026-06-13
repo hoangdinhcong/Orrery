91668628671635aef40429098c836a0949126bf9
hoangdinhcong <cong.hoang@workbuddy.com>
2026-06-13 13:36:09 +0000

    Deploy: solar-system behavior

--- assets/BufferResource-BSNRfr0P.js
+++ assets/BufferResource-BSNRfr0P.js
@@ -1,4 +1,4 @@
-import{w as U,g,c,u as S,B as _,d as m,E as I,e as p}from"./index-rjZl_wPz.js";const x={name:"local-uniform-bit",vertex:{header:`
+import{w as U,g,c,u as S,B as _,d as m,E as I,e as p}from"./index-D9ZWW-3k.js";const x={name:"local-uniform-bit",vertex:{header:`
 
             struct LocalUniforms {
                 uTransformMatrix:mat3x3<f32>,
